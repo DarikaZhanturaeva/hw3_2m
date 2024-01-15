94668c963562fb6ea249a0e3ee0664b6aaa47c68
Darika <darikazhanturaeva@gmail.com>
2024-01-15 15:12:02 +0600

    hw3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        BankAccount bankAccount = new BankAccount(0.0);
+        bankAccount.deposit(15000);
+        while (true) {
+            try {
+                bankAccount.withDraw(6000);
+                System.out.println("Снято 6000, текущий баланс "
+                        + bankAccount.getAmount());
+            } catch (LimitException e) {
+                try {
+                    bankAccount.withDraw(bankAccount.getAmount());
+                } catch (LimitException ex) {
+                    throw new RuntimeException(ex);
+                }
+                System.out.println("Исключение " + e.getMessage());
+                System.out.println("Максимальная сумма для снятия " + e.getRemainingAmount());
+                System.out.println("Cнято " + e.getRemainingAmount());
+                System.out.println("Остаток на счете 0");
+                break;
+            }
+        }
     }
+
 }
